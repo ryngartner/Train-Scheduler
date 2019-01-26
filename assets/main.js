0476e0d95e5f7e63f66cf28ffb79882fab61e918
Ryan Gartner <ryanjgartner@gmail.com>
2019-01-26 09:13:30 -0600

    Additional Code to js and html

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,13 @@
-// Begin Javascript
+// Begin Javascript - Firebase
+
+$(document).ready(function() {
+    var config = {
+        apiKey: "AIzaSyDqoQTWcOsQzKCR2rzuOeTiivWUAZtW_yE",
+        authDomain: "train-scheduler-23d8a.firebaseapp.com",
+        databaseURL: "https://train-scheduler-23d8a.firebaseio.com",
+        projectId: "train-scheduler-23d8a",
+        storageBucket: "",
+        messagingSenderId: "421918923570"
+      };
+      firebase.initializeApp(config)
+};
